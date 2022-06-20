54f9e120065bdbe516ab0c731d3b01f0d7d8f4d6
Eddy <eagle100065@gmail.com>
2022-06-19 20:22:21 -0400

    added list styles to information form api

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 import callBreweryAPI from "../api";
 
+import GoogleMapReact from "google-map-react";
+
 function home() {
   let [currentBrewerys, setCurrentBrewerys] = useState([]);
 
@@ -15,26 +17,35 @@ function home() {
   return (
     <div>
       {currentBrewerys.length > 0 ? (
-        currentBrewerys.map((brewery) => {
-          return (
-            <>
-              <div> {brewery.name} </div>
-              <div> {brewery.brewery_type}</div>
-              <div>
-                {" "}
-                {brewery.street}, {brewery.city}, {brewery.state},{" "}
-                {brewery.postal_code}
-              </div>
-              <div>
-                {" "}
-                <a href={brewery.website_url}> {brewery.website_url}</a>{" "}
-              </div>
-            </>
-          );
-        })
+        <ul className="list-group">
+          {currentBrewerys.map((brewery) => {
+            return (
+              <>
+                <li className="list-group-item">
+                  <div> {brewery.name} </div>
+                  <div> {brewery.brewery_type}</div>
+                  <div>
+                    {" "}
+                    {brewery.street}, {brewery.city}, {brewery.state},{" "}
+                    {brewery.postal_code}
+                  </div>
+                  <div>
+                    {" "}
+                    <a target="_blank" href={brewery.website_url}>
+                      {" "}
+                      {brewery.website_url}
+                    </a>{" "}
+                  </div>
+                </li>
+              </>
+            );
+          })}
+        </ul>
       ) : (
         <div> No current Brewerys available </div>
       )}
+
+      <div> {/*here the map will go  */}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import styles from "../dist/styles.css";
 
 ReactDOM.render( <App />, document.getElementById("App"));
